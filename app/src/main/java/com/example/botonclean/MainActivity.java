4085f3d423489c22de4cf1c625e910181d605560
JulianHomelo <camaronconpan2@gmail.com>
2022-09-14 00:28:49 -0300

    Initial commit

---
+++ app/src/main/java/com/example/botonclean/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.botonclean;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn_sig;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn_sig=(Button)findViewById(R.id.btn_sig);
+
+        btn_sig.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent btn_sig = new Intent(MainActivity.this, activity2_clean.class);
+                startActivity(btn_sig);
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/botonclean/activity2_clean.java
@@ -0,0 +1,14 @@
+package com.example.botonclean;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity2_clean extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity2_clean);
+    }
+}
