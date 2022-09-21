c7b203f7541027aa8ee0342026a51f4f13adc1c5
JulianHomelo <camaronconpan2@gmail.com>
2022-09-21 00:55:02 -0300

    Se terminó completo el diseño, faltan la mayoria de las funcionalidades.

--- app/src/main/java/com/example/botonclean/Activity3_horas.java
+++ app/src/main/java/com/example/botonclean/Activity3_horas.java
@@ -2,19 +2,34 @@ package com.example.botonclean;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.text.BreakIterator;
+import java.util.Calendar;
 
 public class Activity3_horas extends AppCompatActivity {
 
     Button bt_aceptar;
 
+
+    TextView tv;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity3_horas);
+
         bt_aceptar = (Button) findViewById(R.id.bt_aceptar);
 
         bt_aceptar.setOnClickListener(new View.OnClickListener() {
@@ -25,5 +40,28 @@ public class Activity3_horas extends AppCompatActivity {
             }
         });
 
+
+
+
     }
-}
+
+
+    public void abrirHora(View view) {
+
+        Calendar c = Calendar.getInstance();
+        int hora = c.get(Calendar.HOUR_OF_DAY);
+        int minutos = c.get(Calendar.MINUTE);
+
+        TimePickerDialog tmd = new TimePickerDialog(Activity3_horas.this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
+
+                tv.setText(hourOfDay +":"+ minute);
+
+            }
+        }
+                ,hora,minutos,false);
+        tmd.show();
+    }
+
+    }
